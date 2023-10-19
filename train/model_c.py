f945dd0d04631622f2c439cdc42f9f480fbba600
kacper <wwerk001@gold.ac.uk>
2023-10-19 12:45:18 +0200

    initial conditional model and training

---
+++ train/model_c.py
@@ -0,0 +1,105 @@
+from keras import layers
+from keras import optimizers
+from keras.models import Model
+
+def build_spectral_ae(input_shape=(513, 256, 1),
+                      latent_dim=3,
+                      cond_dim=10,
+                      n_filters=[32, 64, 128, 256, 512],
+                      lr=0.001):
+
+    f1, f2, f3, f4 = n_filters[:4]
+
+    input_spect = layers.Input(input_shape)
+    cond_input = layers.Input((cond_dim,))
+
+    # Reshape and tile condition input for concatenation
+    cond_input_reshaped = layers.Reshape((1, 1, cond_dim))(cond_input)
+    cond_input_tiled = layers.UpSampling2D(size=(input_shape[0], input_shape[1]))(cond_input_reshaped)
+
+    # Concatenate condition with spectrogram input
+    x = layers.Concatenate(axis=-1)([input_spect, cond_input_tiled])
+
+    # Encoder
+    x = layers.Conv2D(f1, (5,5), padding='same', strides=(2,2))(input_spect)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(f1, (4,4), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(f1, (4,4), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(f2, (4,4), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(f2, (4,4), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(f2, (4,4), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(f3, (4,4), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(f3, (4,4), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(f3, (4,4), padding='same', strides=(2,1))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(f4, (1,1), padding='same', strides=(2,1))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(latent_dim, (1,1), padding='same', strides=(1,1))(x)
+
+    # Generate z
+    z = layers.BatchNormalization()(x)
+
+    # Decoder
+    input_z = layers.Input(shape=(1, 1, latent_dim))
+    x = layers.Conv2DTranspose(f4, (1,1), padding='same', strides=(1,1))(input_z)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2DTranspose(f3, (2,2), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2DTranspose(f3, (2,2), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2DTranspose(f3, (2,2), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2DTranspose(f2, (2,2), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2DTranspose(f2, (2,2), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2DTranspose(f2, (2,2), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2DTranspose(f1, (2,2), padding='same', strides=(2,2))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2DTranspose(f1, (2,2), padding='same', strides=(2,1))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2DTranspose(f1, (3,1), padding='valid', strides=(2,1))(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
+    x = layers.BatchNormalization()(x)
+    output_spect = layers.Conv2DTranspose(1, (1,1), padding='same', strides=(1,1))(x)
+    
+    encoder = Model([input_spect, cond_input], z)
+    encoder.summary()
+
+    decoder = Model([input_z, cond_input], output_spect)
+    decoder.summary()
+
+    outputs = decoder([encoder([input_spect, cond_input]), cond_input])
+
+    autoencoder = Model([input_spect, cond_input], outputs)
+    autoencoder.compile(optimizer=optimizers.Adam(learning_rate=lr), loss='mean_squared_error')
+    autoencoder.summary()
+    
+    return encoder, decoder, autoencoder
---
+++ train/train_c.py
@@ -0,0 +1,59 @@
+import datetime
+import util
+import model_c
+
+# hyperparameters
+epochs = 200
+batch_size = 8
+learning_rate = 0.0001
+latent_dim = 3
+cond_dim = 32
+n_filters = [32, 64, 128, 1024]
+train_split = 0.8
+n_samples = None
+
+# input
+input_shape = (513, 128, 1)
+rate = 16000
+
+# build the model
+e, d, ae = model_c.build_spectral_ae(input_shape=input_shape, 
+                                   cond_dim=cond_dim,
+                                   latent_dim=latent_dim,
+                                   n_filters=n_filters,
+                                   lr=learning_rate)
+
+# load the data
+x_train, x_test = util.load_specgrams('spectrograms', (513, 128), train_split=train_split, n_samples=n_samples)
+p_train, p_test = util.load_params('params', train_split=train_split, n_samples=n_samples)
+
+start_time = datetime.datetime.today()
+
+# train the thing
+history = ae.fit(x=[x_train, p_train], y=[x_train, p_train],
+                shuffle=True,
+                epochs=epochs,
+                batch_size=batch_size,
+                validation_data=([x_train, p_train], [x_train, p_train]))
+
+end_time = datetime.datetime.today()
+
+print(f"Completed {epochs} in {end_time-start_time}.")
+
+# generate report here
+r = {'start_time' : start_time,
+     'end_time' : end_time,
+     'history' : history.history,
+     'batch_size' : batch_size,
+     'epochs' : epochs,
+     'learning_rate' : learning_rate,
+     'latent_dim' : latent_dim,
+     'n_filters' : n_filters,
+     'input_shape' : input_shape,
+     'rate' : rate,
+     'n_samples' : n_samples,
+     'encoder' : e,
+     'decoder' : d,
+     'autoencoder' : ae}
+
+util.generate_report(r)
