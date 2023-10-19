ac94a3e57702642f8a48faf508176c6fa0aadceb
kacper <wwerk001@gold.ac.uk>
2023-10-19 12:43:52 +0200

    current dir fix

--- train/preprocess.py
+++ train/preprocess.py
@@ -1,4 +1,9 @@
 from util import convert_sample_rate, generate_specgrams
+import os
+
+# Change the working directory to the train folder
+if os.getcwd().split('/')[-1] != 'train':
+    os.chdir('train')
 
 convert_sample_rate('data/MIT_IR_dataset', 'preprocessed_audio', 16000)
 generate_specgrams('preprocessed_audio', 'spectrograms')
