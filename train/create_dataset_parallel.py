114fd67cbf0950c245ebfadf973a7e089597f8db
kacper <wwerk001@gold.ac.uk>
2023-11-12 17:36:59 +0000

    add ben's reverb, cleanups

---
+++ train/create_dataset_parallel.py
@@ -0,0 +1,117 @@
+import warnings
+from numba.core.errors import NumbaPerformanceWarning
+warnings.filterwarnings("ignore", category=NumbaPerformanceWarning)
+import numpy as np                          
+import scipy
+import pandas as pd
+from soundfile import  write
+from dsp_ import prime
+import time
+from dsp_ import simple_fdn
+import os
+from util import generate_specgram, plot_specgram
+from multiprocessing import Pool
+
+# Set the paths
+p_dir = "data_/params"
+y_dir = "data_/audio"
+spect_dir = "data_/spectrograms"
+plot_dir = "data_/spect_plots"
+
+# FDN size and samplerate
+FDN_SIZE = 16
+SAMPLE_RATE = 16000
+IMPULSE_NUM = 20
+MAX_LENGTH = 2000
+PLOT = True
+NUM_WORKERS = os.cpu_count() # default to number of cores, can be set manually
+
+# Create the output directory if it doesn't exist
+os.makedirs(p_dir, exist_ok=True)
+os.makedirs(y_dir, exist_ok=True)
+os.makedirs(spect_dir, exist_ok=True)
+os.makedirs(plot_dir, exist_ok=True)
+
+# Set the random seed for reproducibility
+np.random.seed(42)
+
+# Unipolar impulse
+x = np.zeros(2)
+x[0] = -1
+x[1] = 1
+
+# Hadamard matrix
+H = scipy.linalg.hadamard(FDN_SIZE) * 0.25
+
+# Random parameter arrays
+decay           = np.random.random((IMPULSE_NUM))
+min_dist        = np.random.random((IMPULSE_NUM)) * 0.1
+max_dist        = 0.1 + np.random.random((IMPULSE_NUM)) * 0.9
+distance_curve  = np.random.random((IMPULSE_NUM))
+min_freq        = np.random.random((IMPULSE_NUM)) * 0.5
+max_freq        = 0.5 + np.random.random((IMPULSE_NUM)) * 0.5
+frequency_curve = np.random.random((IMPULSE_NUM))
+
+parameters = pd.DataFrame(
+    {
+        "Decay":           decay,
+        "Min distance":    min_dist,
+        "Max distance":    max_dist,
+        "Distance curve":  distance_curve,
+        "Low frequency":   min_freq,
+        "High frequency":  max_freq,
+        "Frequency curve": frequency_curve
+    }
+)
+
+# prime number list for delay line
+PRIME_LIST = prime(0, 30000).astype(np.int32)
+
+# 2 second impulse responses
+IMPULSE_LENGTH = (SAMPLE_RATE * 2) + x.shape[-1]
+
+def generate_impulse(i, params, H, PRIME_LIST, SAMPLE_RATE, MAX_LENGTH):
+    y = simple_fdn(x,
+                    decay=params[0],
+                    min_dist=params[1],
+                    max_dist=params[2],
+                    distance_curve=params[3],
+                    min_freq=params[4],
+                    max_freq=params[5],
+                    frequency_curve=params[6],
+                    H=H,
+                    prime_list=PRIME_LIST,
+                    sr=SAMPLE_RATE,
+                    max_length=MAX_LENGTH)
+    Sn = np.array(generate_specgram(y, SAMPLE_RATE))
+    return (i, y, Sn)
+
+
+def main():
+    start_time = time.time()
+
+    with Pool(NUM_WORKERS) as pool:
+        results = pool.starmap(generate_impulse, [(i, parameters.values[i], H, PRIME_LIST, SAMPLE_RATE, MAX_LENGTH) for i in range(IMPULSE_NUM)])
+
+    for i, y, Sn in results:
+    # Save impulse response audio
+        y_path = y_dir + '/' + "impulse" + f"_{i}.wav"
+        write(y_path, y, SAMPLE_RATE)
+        # Save FDN params
+        p_path = p_dir + '/' + "impulse" + f"_{i}"
+        # print(parameters.values[i])
+        np.savetxt(p_path, parameters.values[i])
+        # Generate and save the spectrogram
+        spect_path = spect_dir + '/' + "impulse" + f"_{i}"
+        np.savetxt(spect_path, Sn)
+        # Optional: Generate and save spectrogram plot
+        if PLOT:
+            plot_specgram(Sn, SAMPLE_RATE, "impulse" + f"_{i}", plot_dir)
+        print(f"{i+1}/{IMPULSE_NUM}")
+
+    end_time = time.time()
+    runtime = end_time - start_time
+    print(f"Finished in: {runtime} seconds")
+
+if __name__ == "__main__":
+    main()
--- train/util.py
+++ train/util.py
@@ -12,6 +12,11 @@ import resampy
 from tqdm import tqdm
 import soundfile as sf
 from scipy.stats import norm
+import numba
+import numpy as np
+from librosa import stft, power_to_db, display
+from matplotlib import pyplot as plt
+import os
 
 import matplotlib as mpl 
 mpl.use('agg')
@@ -672,3 +677,94 @@ def ispecgram(spec,
     else:
         audio = librosa.core.istft(spec_real, **ifft_config)
     return np.squeeze((audio / audio.max()) * 0.25) # scale to -12dB peak
+
+###
+# Single file versions from original repo by C. Steinmetz
+###
+def generate_specgram(x, sr=16000, n_fft=1024, n_hop=256):
+    """
+    Generate a spectrogram (via stft) on input audio data.
+
+    Args:
+        x (ndarray): Input audio data.
+        sr (int, optional): Sample rate out input audio data.
+        n_fft (int, optional): Size of the FFT to generate spectrograms.
+        n_hop (int, optional): Hop size for FFT.
+    """
+    S = stft(x, n_fft=n_fft, hop_length=n_hop, center=True)
+    power_spectra = np.abs(S)**2
+    log_power_spectra = power_to_db(power_spectra)
+    _min = np.amin(log_power_spectra)
+    _max = np.amax(log_power_spectra)
+    if _min == _max:
+        print(f"divide by zero in audio array")
+    else:
+        normalized_log_power_spectra = (log_power_spectra - _min) / (_max - _min)
+    return normalized_log_power_spectra
+    
+
+def plot_specgram(log_power_spectra, rate, filename, output_dir):
+    """ 
+    Save log-power and normalized log-power specotrgram to file.
+
+    Args:
+        log_power_spectra (ndarray): Comptued Log-Power spectra.
+        rate (int): Sample rate of input audio data.
+        filename (str): Output filename for generated plot.
+        output_dir (str): Directory to save generated plot.
+    """
+
+    plt.figure()
+    display.specshow(log_power_spectra, sr=rate*2, y_axis='log', x_axis='time')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Log-Power spectrogram')
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_dir, filename))
+    plt.close('all')
+
+    ###
+    # Other misc functions
+    ###
+    @numba.jit(nopython=True)
+def pad(a, pad_width, constant_values=0):
+    """
+    Pads a 1D NumPy array with a constant value.
+
+    Parameters:
+    a (ndarray): Input array.
+    pad_width (int or sequence of ints): Number of values padded to the edges of each axis.
+    constant_values (scalar or sequence): Values used for padding. Default is 0.
+
+    Returns:
+    ndarray: Padded array.
+
+    Note:
+    This function works only for 1D arrays.
+    """
+    a_ = np.zeros(a.shape[-1] + pad_width)
+    a_[:a.shape[-1]] = a
+    return a_
+
+@numba.jit(nopython=True)
+def padadd(a, b):
+    """
+    Adds two 1D NumPy arrays, padding the shorter array if their lengths do not match.
+
+    Parameters:
+    a (ndarray): First input array.
+    b (ndarray): Second input array.
+
+    Returns:
+    ndarray: Sum of the two arrays with padding.
+
+    Note:
+    This function works only for 1D arrays.
+    """
+    len_a = a.shape[-1]
+    len_b = b.shape[-1]
+    max_len = max(len_a, len_b)
+    if len_a < max_len:
+        a = pad(a, max_len - len_a)
+    if len_b < max_len:
+        b = pad(b, max_len - len_b)
+    return a + b
